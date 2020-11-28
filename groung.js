2a101fa496cdb9c910b257600cd7517238de525d
Aryanyerolkar123 <74817573+Aryanyerolkar123@users.noreply.github.com>
2020-11-28 17:09:13 +0530

    Add files via upload

---
+++ groung.js
@@ -0,0 +1,22 @@
+class ground {
+    constructor(x, y, width, height) {
+        var options = {
+            friction:10,
+            density:10
+        }
+        this.body = Bodies.rectangle(this.x, this.y, this.width, this.height)
+        this.width = width
+        this.height = height
+    }
+    display() {
+        var pos = this.body.position
+        var angle = this.body.angle
+        push()
+        translate(pos.x = pos.y)
+        rotate()
+        rect(x, y, this.width, this.hieght)
+    }
+}
+    
+    
+    
